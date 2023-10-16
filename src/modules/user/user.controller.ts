8a110f51f85912cb4cbd0828cf26b068cef0ed43
Amruth-Vamshi <amruth.vamshi@esmagico.in>
2023-10-16 11:59:19 +0530

    added user conversation history APIs.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Param, Query, Request } from '@nestjs/common';
+import { Controller, Get, Param, Query, Request, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { query } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 import { CustomLogger } from '../../common/logger';
+import { AuthGuard } from 'src/common/auth-gaurd';
 const fetch = require('node-fetch'); 
 const { Headers } = fetch;
 
 @Controller('user')
+@UseGuards(AuthGuard)
 export class UserController {
   private logger: CustomLogger;
   constructor(
@@ -20,55 +22,43 @@ export class UserController {
   async conversations(
     @Request() request, 
     @Query('userid') adminUserId: string, 
-    @Query('page') page: number, 
-    @Query('perPage') perPage: number,
-    @Query('mobileNumber') mobileNumber: string,
-    @Query('fromDate') fromDate: string,
-    @Query('toDate') toDate: string
+    @Query('mobileNumber') mobileNumber: string
   ): Promise<query[]> {
     let userId = null
-    userId = adminUserId
-    // if(request.headers.roles.indexOf('Admin') != -1) {
-    //   userId = adminUserId
-    //   if(!userId && mobileNumber) {
-    //     var myHeaders = new Headers();
-    //     myHeaders.append("x-application-id", this.configService.get("FRONTEND_APPLICATION_ID"));
-    //     myHeaders.append("Authorization", this.configService.get('FUSION_AUTH_API_KEY'));
+    if(request.headers.roles.indexOf('Admin') != -1) {
+      userId = adminUserId
+      if(!userId && mobileNumber) {
+        var myHeaders = new Headers();
+        myHeaders.append("x-application-id", this.configService.get("FRONTEND_APPLICATION_ID"));
+        myHeaders.append("Authorization", this.configService.get('FUSION_AUTH_API_KEY'));
 
-    //     var requestOptions: RequestInit = {
-    //       method: 'GET',
-    //       headers: myHeaders,
-    //       redirect: 'follow'
-    //     };
-    //     try{
-    //       let res: any = await fetch(`${this.configService.get('FUSION_AUTH_BASE_URL')}api/user?username=${mobileNumber}`, requestOptions)
-    //       res = await res.json()
-    //       userId = res.user.id
-    //     } catch(error) {
-    //       this.logger.error(error)
-    //     }
-    //   }
-    // } else {
-    //   userId = request.headers.userId
-    // }
-    page = page?page:1
-    perPage = perPage?perPage:10
+        var requestOptions: any = {
+          method: 'GET',
+          headers: myHeaders,
+          redirect: 'follow'
+        };
+        try{
+          let res: any = await fetch(`${this.configService.get('FUSION_AUTH_BASE_URL')}api/user?username=${mobileNumber}`, requestOptions)
+          res = await res.json()
+          userId = res.user.id
+        } catch(error) {
+          this.logger.error(error)
+        }
+      }
+    } else {
+      userId = request.headers.userId
+    }
     return this.userService.conversationsList(
-      userId,
-      parseInt(`${page}`),
-      parseInt(`${perPage}`),
-      fromDate,
-      toDate
+      userId
     );
   }
 
   @Get("/chathistory/:conversationId")
   async chatHistory(@Param("conversationId") conversationId: string, @Request() request, @Query('userid') adminUserId: string): Promise<query[]> {
-    let userId = adminUserId
-    // let userId = request.headers.userId
-    // if(request.headers.roles.indexOf('Admin') != -1) {
-    //   userId = adminUserId
-    // }
+    let userId = request.headers.userId
+    if(request.headers.roles.indexOf('Admin') != -1) {
+      userId = adminUserId
+    }
     return this.userService.conversationHistory(conversationId,userId);
   }
 
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -18,108 +18,6 @@ export class UserService {
     this.logger = new CustomLogger("UserService");
   }
 
-  async conversationsList(
-    userId: string, 
-    page: number, 
-    perPage: number,
-    fromDate: string,
-    toDate: string
-  ): Promise<any> {
-    if (
-      fromDate &&
-      !moment(fromDate, "YYYY-MM-DD HH:mm:ss", true).isValid()
-    ) {
-      throw new BadRequestException("Invalid 'fromDate' format. Please provide it in 'YYYY-MM-DD HH:mm:ss' format.");
-    }
-
-    if (toDate && !moment(toDate, "YYYY-MM-DD HH:mm:ss", true).isValid()) {
-      throw new BadRequestException("Invalid 'toDate' format. Please provide it in 'YYYY-MM-DD HH:mm:ss' format.");
-    }
-    try {
-      let whereClause = '';
-
-      if (fromDate) {
-        const utcFromDateTime = momentTZ
-          .tz(fromDate, "YYYY-MM-DD HH:mm:ss", "Asia/Kolkata")
-          .utc()
-          .format();
-        whereClause += `m."updatedAt" >= '${utcFromDateTime}'::timestamptz`;
-      }
-
-      if (toDate) {
-        const utcToDateTime = momentTZ
-          .tz(toDate, "YYYY-MM-DD HH:mm:ss", "Asia/Kolkata")
-          .utc()
-          .format();
-        whereClause += `${fromDate ? ' AND ' : ''}m."updatedAt" <= '${utcToDateTime}'::timestamptz`;
-      }
-
-      let userHistory: any = await this.prisma.$queryRawUnsafe(`
-        SELECT DISTINCT ON (q."conversationId") q.*, m."updatedAt" AS "lastConversationAt"
-        FROM (
-          SELECT *
-          FROM "query"
-          WHERE ${userId ? `"userId" = '${userId}' AND `:''}"isConversationDeleted" = false
-        ) q
-        INNER JOIN "query" m ON q."conversationId" = m."conversationId"
-        WHERE m."updatedAt" = (
-          SELECT MAX("updatedAt")
-          FROM "query"
-          WHERE "conversationId" = q."conversationId"
-        )
-        ${whereClause ? `AND ${whereClause}` : ''}
-        ORDER BY q."conversationId" ASC, q."createdAt" ASC
-        OFFSET ${(page - 1) * perPage} LIMIT ${perPage}
-      `);
-
-      // userHistory = await Promise.all(userHistory.map( async (message) => {
-      //   //get users mobile number
-      //   var myHeaders = new Headers();
-      //   myHeaders.append("x-application-id", this.configService.get('FRONTEND_APPLICATION_ID'));
-      //   myHeaders.append("Authorization", this.configService.get('FUSION_AUTH_API_KEY'));
-      //   var requestOptions: RequestInit = {
-      //     method: 'GET',
-      //     headers: myHeaders,
-      //     redirect: 'follow'
-      //   };
-      //   try {
-      //     let res: any = await fetch(`${this.configService.get('FUSION_AUTH_BASE_URL')}api/user?userId=${message.userId}`, requestOptions)
-      //     res = await res.json()
-      //     message['mobileNumber'] = res.user.mobilePhone
-      //   } catch(error) {
-      //     this.logger.error(error)
-      //     message['mobileNumber'] = null
-      //   }
-      //   return message
-      // }))
-
-      const conversations: any =  await this.prisma.$queryRawUnsafe(`
-        SELECT DISTINCT ON (q."conversationId") q.*, m."updatedAt" AS "lastConversationAt"
-        FROM (
-          SELECT *
-          FROM "query"
-          WHERE ${userId ? `"userId" = '${userId}' AND `:''}"isConversationDeleted" = false
-        ) q
-        INNER JOIN "query" m ON q."conversationId" = m."conversationId"
-        WHERE m."updatedAt" = (
-          SELECT MAX("updatedAt")
-          FROM "query"
-          WHERE "conversationId" = q."conversationId"
-        )
-        ${whereClause ? `AND ${whereClause}` : ''}
-        ORDER BY q."conversationId" ASC, q."createdAt" ASC
-      `);
-      const totalConversations = conversations.length;
-      const totalPages = Math.ceil(totalConversations / perPage);
-      const pagination = { page, perPage, totalPages, totalConversations };
-      return {pagination, userHistory};
-    } catch (error) {
-      throw new BadRequestException([
-        "Something went wrong while fetching user history",
-      ]);
-    }
-  }
-
   async conversationHistory(
     conversationId: string,
     userId: string
@@ -175,6 +73,34 @@ export class UserService {
       return employee
     } catch(error) {
         return null
+    }
+  }
+
+  async conversationsList(
+    userId: string,
+  ): Promise<any> {
+    try {
+      let userHistory: any = await this.prisma.$queryRawUnsafe(`
+        SELECT DISTINCT ON (q."conversationId") q.*, m."updatedAt" AS "lastConversationAt"
+        FROM (
+          SELECT *
+          FROM "query"
+          WHERE ${userId ? `"userId" = '${userId}' AND `:''}"isConversationDeleted" = false
+        ) q
+        INNER JOIN "query" m ON q."conversationId" = m."conversationId"
+        WHERE m."updatedAt" = (
+          SELECT MAX("updatedAt")
+          FROM "query"
+          WHERE "conversationId" = q."conversationId"
+        )
+        ORDER BY q."conversationId" ASC, q."createdAt" ASC
+      `);
+      return userHistory;
+    } catch (error) {
+      console.log(error)
+      throw new BadRequestException([
+        "Something went wrong while fetching user history",
+      ]);
     }
   }
 }
